53474914311b4e15b6f0a5db64af02221d802000
Ryan Routsong <routsongrm@nih.gov>
2024-07-17 13:23:11 -0400

    fix: account for blank RNA column

--- src/run.py
+++ src/run.py
@@ -702,7 +702,8 @@ def valid_input(sheet):
         row['DNA'] = os.path.abspath(row['DNA'])
         if not os.path.exists(row['DNA']):
             raise argparse.ArgumentTypeError(f"Sample sheet path `{row['DNA']}` does not exist")
-        if 'RNA' in row:
+        if 'RNA' in row and not row['RNA'] in ('', None, 'None'):
+            import ipdb; ipdb.set_trace()
             RNA_included = True
             row['RNA'] = os.path.abspath(row['RNA'])
             if not os.path.exists(row['RNA']):
